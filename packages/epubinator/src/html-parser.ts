0977faa6a880dc3f3c21d571923cddda55a989c5
Pravin Bashyal <pravin.bashyal@gmail.com>
2020-02-26 17:12:50 +0100

    restore deleted functions

--- packages/epubinator/src/html-parser.ts
+++ packages/epubinator/src/html-parser.ts
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch'
 import { JSDOM } from 'jsdom'
+import { ContextType } from './models/ContextType'
 import { getDocument } from './util/jsdom'
 import { log, info, success, lineBreak, emphasizedInfo } from './logger'
 
@@ -14,6 +15,27 @@ export async function getDom(url: string): Promise<JSDOM> {
   const html = await response.text()
   const dom = new JSDOM(html)
   return dom
+}
+
+/**
+ * getArticle
+ *
+ * @param dom
+ * @param context
+ * @returns JSDOM object
+ */
+export function getArticle(dom: JSDOM, context: ContextType = {}): JSDOM {
+  const document = getDocument(dom)
+  const article =
+    document.querySelector('article') || document.querySelector('body')
+  if (!article) {
+    throw new Error(
+      `cannot find article. describe the article explicitly ${JSON.stringify(
+        context
+      )}`
+    )
+  }
+  return new JSDOM(article.outerHTML)
 }
 
 export function removeToc(dom: JSDOM): JSDOM {
@@ -32,6 +54,23 @@ export function removeToc(dom: JSDOM): JSDOM {
 export function getBodyHtmlFromDom(dom: JSDOM): string {
   if (!dom) return ''
   return dom.window.document.querySelector('body').innerHTML
+}
+
+/**
+ * getTitle
+ *
+ * @param {JSDOM} dom
+ * @param {ContextType} context={}
+ * @returns {string}
+ */
+export function getTitle(dom: JSDOM, context: ContextType = {}): string {
+  const document = getDocument(dom)
+  const titleElement = document.querySelector('h1')
+  if (!titleElement) {
+    console.log(`cannot find title at ${context.url}`)
+    return ''
+  }
+  return titleElement.innerHTML
 }
 
 /**
@@ -54,6 +93,31 @@ export function removeTitle(dom: JSDOM): JSDOM {
 function getFallbackTitleContent(dom: JSDOM): HTMLElement {
   const title = getDocument(dom).querySelector('h1')
   return title.parentElement
+}
+
+/**
+ * getMain
+ *
+ * @param {JSDOM} dom
+ * @param {ContextType} context={}
+ * @returns {JSDOM}
+ */
+export function getMain(dom: JSDOM, context: ContextType = {}): JSDOM {
+  const document = getDocument(dom)
+  const fallbackTitleContent = getFallbackTitleContent(dom)
+  const main =
+    document.querySelector('main') ||
+    document.querySelector('.content') ||
+    document.querySelector("[role='main']") ||
+    document.querySelector('#main') ||
+    fallbackTitleContent ||
+    document.querySelector('body')
+  if (!main) {
+    throw new Error(
+      `cannot find main. describe the main explicitly at ${context.url} ${dom.window.document.documentElement.outerHTML}`
+    )
+  }
+  return new JSDOM(main.outerHTML)
 }
 
 /**
