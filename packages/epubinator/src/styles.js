1641b06f2dc562b616fbca80b2d28bbeea551e4a
Pravin Bashyal <pravin.bashyal@gmail.com>
2020-02-26 16:34:48 +0100

    remove built js files

--- packages/epubinator/src/styles.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = require("fs");
-console.log();
-var content = fs_1.readFileSync(__dirname + '/textfiles/stylestext.css', {
-    encoding: 'utf-8',
-});
-exports.default = content;
--- packages/epubinator/src/util/jsdom.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getDocument(dom) {
-    var document = dom.window.document;
-    return document;
-}
-exports.getDocument = getDocument;
