3311f22d9b770c2e05ab37f9a4da5be30eeaf7af
Pravin Bashyal <pravin.bashyal@gmail.com>
2020-12-29 14:40:07 +0100

    use output file

--- packages/epubinator/src/paginator.ts
+++ packages/epubinator/src/paginator.ts
@@ -8,8 +8,13 @@ const getNextPageLink = async (
   const {
     window: { document },
   } = dom
-  const next: HTMLLinkElement | undefined =
-    document.querySelector('[rel="next"]') || document.querySelector('.next')
+  const next: HTMLLinkElement | HTMLAnchorElement | undefined =
+    document.querySelector('[rel="next"]') ||
+    document.querySelector('.next') ||
+    document.querySelector('[name="next"]') ||
+    document.querySelector('[name="NEXT"]') ||
+    document.querySelector('[name="Next"]') ||
+    linkWithInnerTextNext(document)
   if (!next) {
     return
   }
@@ -24,4 +29,32 @@ const getNextPageLink = async (
 
 export { getNextPageLink }
 
-const a = new URLSearchParams()
+const linkWithInnerTextNext = (
+  document: Document
+): HTMLAnchorElement | undefined => {
+  const nextElement = Array.from(
+    document.querySelectorAll('p, a, span, div')
+  ).find(elementWithNextInTree)
+  return nextElement && nextElement.closest('a')
+}
+
+const leafHasNext = (node: Element) => {
+  const hasText = (nodeCur: Element) => /next/i.test(nodeCur.textContent)
+  const nodeHasText = hasText(node)
+  const childrenDontHaveText = Array.from(node.children).every(
+    (child) => !hasText(child)
+  )
+
+  return nodeHasText && childrenDontHaveText
+}
+
+function elementWithNextInTree(element: Element) {
+  return (
+    leafHasNext(element) &&
+    element.textContent
+      .split(' ')
+      .find((text) => text.toLowerCase() === 'next') &&
+    element.textContent.split(' ').length < 4 &&
+    element.closest('a')
+  )
+}
